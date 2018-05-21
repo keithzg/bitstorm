cb0bdf60e5c5fc919efd533649fe59be327a008b
keithzg <keithzg@gmail.com>
2018-05-21 16:49:45 -0600

    Push config out to separate file, so that the main code can be updated from the repo more seamlessly

--- announce.php
+++ announce.php
@@ -22,24 +22,7 @@
  ** Configuration start **
  *************************/
 
-//MySQL details
-define('__DB_SERVER', '');
-define('__DB_USERNAME', '');
-define('__DB_PASSWORD', '');
-define('__DB_DATABASE', '');
-
-//Peer announce interval (Seconds)
-define('__INTERVAL', 1800);
-
-//Time out if peer is this late to re-announce (Seconds)
-define('__TIMEOUT', 120);
-
-//Minimum announce interval (Seconds)
-//Most clients obey this, but not all
-define('__INTERVAL_MIN', 60);
-
-// By default, never encode more than this number of peers in a single request
-define('__MAX_PPR', 20);
+require_once config.php;
 
  /***********************
  ** Configuration end **
